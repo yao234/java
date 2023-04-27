4e21ab2d215ecef5d6cc4a0aa8ad71034e164f7e
yao234 <54147058+yao234@users.noreply.github.com>
2023-04-27 09:27:22 +0800

    Add files via upload

--- AuthenticationInterceptor.java
+++ AuthenticationInterceptor.java
@@ -12,13 +12,17 @@ import com.bwzn.xssmg.Result;
 import com.bwzn.xssmg.annotation.PassToken;
 import com.bwzn.xssmg.annotation.UserLoginToken;
 import com.bwzn.xssmg.entities.BaseStaff;
+import com.bwzn.xssmg.entities.SysView;
 import com.bwzn.xssmg.entities.http.resp.CabinetInfo;
 import com.bwzn.xssmg.help.AccessLimit;
+import com.bwzn.xssmg.help.Manager.ApiManager;
 import com.bwzn.xssmg.help.SSOHelp;
 import com.bwzn.xssmg.service.BaseStaffService;
 import com.bwzn.xssmg.service.CabinetService;
 import com.bwzn.xssmg.service.RedisCacheService;
+import com.bwzn.xssmg.service.SysViewService;
 import com.bwzn.xssmg.utils.HttpsUtils;
+import com.bwzn.xssmg.utils.MD5Utils;
 import okhttp3.Call;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -51,6 +55,9 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
     BaseStaffService baseStaffService;
 
     @Autowired
+    SysViewService sysViewService;
+
+    @Autowired
     CabinetService cabinetService;
 
     //@Autowired
@@ -70,14 +77,9 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object object) throws Exception {
-
-        System.out.println("ok");
-        String requestUrl=httpServletRequest.getHeader("xs-location-href");
-        System.out.println(requestUrl);
         CheckHttpRequest(httpServletRequest,httpServletResponse);
-
-
-        //获取用户访问的超链接
+//        String servletPath = httpServletRequest.getRequestURI();
+//        System.out.println("访问此网站的接口:"+servletPath);
         String token = httpServletRequest.getHeader("Authorization");// 从 http 请求头中取出 token
         // 如果不是映射到方法直接通过
         if (!(object instanceof HandlerMethod)) {
@@ -117,7 +119,11 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
                 if (StringUtils.isEmpty(user_IDNO)){
                     throw new RuntimeException("用户不存在，请重新登录");
                 }
-
+                BaseStaff oneStaff = baseStaffService.findOneStaff(user_IDNO);
+                if (oneStaff.getF_EnabledMark()==0){
+                    throw new Exception("该用户已被禁用，请联系管理员");
+                }
+                CheckPower(oneStaff,httpServletRequest,httpServletResponse);
                 // 获取 token 中的 user id
                 /*String userId;
                 try {
@@ -138,6 +144,8 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
                 }*/
                 return true;
             }
+        }else{
+            CheckPower(null,httpServletRequest,httpServletResponse);
         }
         return true;
     }
@@ -152,9 +160,8 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
 
     }
 
-
+    //访问检查
     private void CheckHttpRequest(HttpServletRequest httpServletRequest,HttpServletResponse httpServletResponse) throws IOException {
-
         //String trustIp="192.168.0.103;192.168.0.95;127.0.0.1";
         /***
          * Http头部参数检查
@@ -202,6 +209,10 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
                 throw new RuntimeException("非白名单站点");
             }
         }
+
+        if (refer!=null && refer.contains("/swagger-ui/index.html")){
+            throw new RuntimeException("非法访问");
+        }
         //***Referer检查***//*
         String contentType=httpServletRequest.getContentType();
 
@@ -227,12 +238,6 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
                 throw new RuntimeException("非法输入");
             }
         }
-//
-//            *//**内容检查**//*
-//            ServletInputStream inputStream=httpServletRequest.getInputStream();
-//           if(inputStream.isReady()){
-//
-//           }*/
 
 
         /**限流   1秒只允许登录请求3次 **/
@@ -302,6 +307,98 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
 
     }
 
+    //检查权限
+    private void CheckPower(BaseStaff staff,HttpServletRequest request,HttpServletResponse httpServletResponse){
+        String requestURI = request.getRequestURI();
+        System.out.println("访问的接口名:"+requestURI);
+
+        //如果没有登陆 只允许用户访问登陆相关的接口
+        if (staff==null) {
+            if (!ApiManager.AccessFlag("Common", requestURI)) {
+                throw new RuntimeException("XS01:无权限访问资源，请联系管理员");
+            }else{
+                return;
+            }
+        }
+        //查询人员信息
+        if (requestURI.equals("/api/staff/queryLoginInfo")){
+            if (!ApiManager.AccessFlag("WebServer",requestURI)){
+                throw new RuntimeException("XS13:该接口未开放");
+            }else{
+                return;
+            }
+        }
+        String header = request.getHeader("xs-location-href");
+        System.out.println("http传过来的自定义参数:"+header);
+
+        //菜单数据
+        if (header==null && requestURI.equals("/api/authority/getViews")){
+            if (!ApiManager.AccessFlag("WebServer",requestURI)){
+                throw new RuntimeException("XS12:该接口未开放");
+            }else{
+                return;
+            }
+        }
+        ////自定义参数带有这个值表示非网页
+        if (header==null || StringUtils.isEmpty(header)){
+            throw new RuntimeException("XS02:无权限访问此接口,请联系管理员");
+        }
+        //双方自行规定即可 安卓端
+        if (header.equals("B48E3F8E7F30A62DF7D655FB0EAB732F")){
+            if (!ApiManager.AccessFlag(header,requestURI)){
+                throw new RuntimeException("XS06:无权限访问此接口资源,请联系管理员");
+            }else {
+                return;
+            }
+        }
+        else if (header.equals("8516D1F5D6A82B40E508DA10E4F98AF9")){ //如果是首页的话 判断接口参数是否带有home
+            if (!requestURI.contains("home")){
+                throw new RuntimeException("XS07:非法访问接口资源");
+            }
+            if (!ApiManager.AccessFlag("WebServer",requestURI)){
+                throw new RuntimeException("XS08:无权限访问此接口，请联系管理员");
+            }
+            else {
+                return;
+            }
+        }
+        else if (header.equals("0D550B0847916C145737A2E1A9F91D3B")){
+            if (!ApiManager.AccessFlag(header,requestURI)){
+                throw new RuntimeException("XS11:无权限访问此接口，请联系管理员");
+            }
+            else {
+                return;
+            }
+        }
+
+
+
+        //如果自定义的都没有包含 则可能是网页端，由于网页端是根据超链接进行MD5加密，所以需要先判断此账户是否有权限访问对应的页面，如果有要判断是否有权限能访问已授权的接口
+        List<String> list = sysViewService.queryAccessPage(staff.getF_IDNO());
+        if (list!=null && list.size()>0){
+            boolean isContinue=false;
+            for (String item:list){
+                String s = MD5Utils.stringToMD5(item); //小写 传过来是32位大写
+                if (s.toUpperCase().equals(header)){
+                    isContinue=true;
+                    break;
+                }
+            }
+            if (!isContinue){
+                throw new RuntimeException("XS03:无权限访问此页面,请联系管理员");
+            }
+            else{
+                //如果允许访问网页还需要判断是否允许访问该接口
+               if (!ApiManager.AccessFlag(header,requestURI)){
+                   throw new RuntimeException("XS05:无权限访问此接口资源，请联系管理员");
+               }
+            }
+
+        }else{
+            throw new RuntimeException("XS04:无权限访问此页面，请联系管理员");
+        }
+    }
+
     public static boolean validateCabinetIp(String ip){
         for (CabinetInfo info : cacheCabinetInfos){
             if (ip.equals(info.getF_IP())){
@@ -365,5 +462,8 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
         }
         return ip;
     }
+
+
+
 
 }
